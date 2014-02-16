a542b1af26dea7de1987fe2904e41aa51ede4d4a
osen <kpedersen@live.co.uk>
2014-02-16 13:27:34 -0800

    Added drink animation.

--- src/drunken/Horse.cpp
+++ src/drunken/Horse.cpp
@@ -51,9 +51,37 @@ void Horse::addCowboy()
   cowboyLGo->getTransform()->setLocalRotation(Vector3(0, 90, 0));
   cowboyLGo->getTransform()->setLocalPosition(Vector3(0, 1.2f, 0));
 
+  Animation* drinkAnimation = Resources::load<Animation>("models/cowboy/drink.anm");
+  mr->setAnimation(drinkAnimation);
+  mr->play();
+
   Animation* sitAnimation = Resources::load<Animation>("models/cowboy/sit.anm");
   lmr->setAnimation(sitAnimation);
   lmr->play();
+
+  Mesh* bmesh = Resources::load<Mesh>("models/bottle/bottle");
+  Texture2d* tex = Resources::load<Texture2d>("models/bottle/bottle");
+
+  GameObject* bGo = new GameObject();
+
+  for(int i = 0; i < mr->getRoot()->getTransform()->getChildCount(); i++)
+  {
+    if(mr->getRoot()->getTransform()->getChild(i)->getGameObject()->getName() == "RightLowerArm")
+    {
+      bGo->getTransform()->setParent(mr->getRoot()->getTransform()->getChild(i));
+      bGo->getTransform()->setLocalRotation(Vector3(0, 0, 0));
+      bGo->getTransform()->setLocalPosition(Vector3(-0.2f, -0.8f, -0.2f));
+    }
+  }
+
+  MeshRenderer* bmr = bGo->addComponent<MeshRenderer>();
+  MeshFilter* bmf = bGo->addComponent<MeshFilter>();
+
+  Material* bmaterial = new Material(Resources::load<Material>("shaders/textured"));
+  bmaterial->setMainTexture(tex);
+
+  bmr->setMaterial(bmaterial);
+  bmf->setMesh(bmesh);
 
 }
 
