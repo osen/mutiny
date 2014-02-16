b7b22b5a53e6f4bdc6a498c9acea97a539750554
osen <kpedersen@live.co.uk>
2014-02-16 11:08:29 -0800

    Added player sitting on horse.

--- src/drunken/Horse.cpp
+++ src/drunken/Horse.cpp
@@ -29,6 +29,32 @@ void Horse::onAwake()
   mr->play();
 
   getGameObject()->getTransform()->setPosition(Vector3(0, -0.5f, 0));
+
+  addCowboy();
+}
+
+void Horse::addCowboy()
+{
+  GameObject* cowboyGo = new GameObject();
+  cowboyGo->getTransform()->setParent(getGameObject()->getTransform());
+  AnimatedMeshRenderer* mr = cowboyGo->addComponent<AnimatedMeshRenderer>();
+  AnimatedMesh* mesh = Resources::load<AnimatedMesh>("models/cowboy/cowboy");
+  mr->setAnimatedMesh(mesh);
+  cowboyGo->getTransform()->setLocalRotation(Vector3(0, 90, 0));
+  cowboyGo->getTransform()->setLocalPosition(Vector3(0, 1.2f, 0));
+
+  GameObject* cowboyLGo = new GameObject();
+  cowboyLGo->getTransform()->setParent(getGameObject()->getTransform());
+  AnimatedMeshRenderer* lmr = cowboyLGo->addComponent<AnimatedMeshRenderer>();
+  AnimatedMesh* lmesh = Resources::load<AnimatedMesh>("models/cowboy/cowboy_lower");
+  lmr->setAnimatedMesh(lmesh);
+  cowboyLGo->getTransform()->setLocalRotation(Vector3(0, 90, 0));
+  cowboyLGo->getTransform()->setLocalPosition(Vector3(0, 1.2f, 0));
+
+  Animation* sitAnimation = Resources::load<Animation>("models/cowboy/sit.anm");
+  lmr->setAnimation(sitAnimation);
+  lmr->play();
+
 }
 
 void Horse::onUpdate()
--- src/drunken/Horse.h
+++ src/drunken/Horse.h
@@ -21,6 +21,8 @@ private:
   AnimatedMeshRenderer* mr;
   Animation* runAnimation;
 
+  void addCowboy();
+
 };
 
 #endif
