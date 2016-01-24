e60e4c6d76c3c4af9deed697487974080c0b3f27
osen <kpedersen@live.co.uk>
2016-01-24 22:25:16 +0000

    Added nomutiny_sdl test project.

---
+++ samples/nomutiny_sdl/assets/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+
+class Component
+{
+public:
+  Component()
+  {
+    std::cout << "Component" << std::endl;
+  }
+
+  virtual ~Component()
+  {
+    std::cout << "~Component" << std::endl;
+  }
+};
+
+class TestComponent : public Component
+{
+public:
+  TestComponent()
+  {
+    std::cout << "TestComponent" << std::endl;
+  }
+
+  virtual ~TestComponent()
+  {
+    std::cout << "~TestComponent" << std::endl;
+  }
+};
+
+
+int main(int argc, char* argv[])
+{
+  std::cout << "Hello World" << std::endl;
+
+  Component* tc = new(calloc(1, sizeof(*tc))) TestComponent();
+  tc->~Component();
+  free(tc);
+
+  return 0;
+}
+
